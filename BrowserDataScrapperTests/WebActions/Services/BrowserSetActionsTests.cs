b4923aa892eda3452b8f41f25e67f5b4e1aa07c3
MichalBudz1993 <m.budziszewski93@gmail.com>
2025-08-01 14:42:53 +0200

    tests(SetActions): Added unit test

---
+++ BrowserDataScrapperTests/WebActions/Services/BrowserSetActionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowserDataScrapper.AutomationActions.Services;
+using BrowserDataScrapper.AutomationActions;
+
+namespace BrowserDataScrapper.WebActions.Services.Tests
+{
+    [TestClass()]
+    public class BrowserSetActionsTests
+    {
+        EngineActions engineActions = new EngineActions();
+        UIActions uiActions = new UIActions();
+
+        [TestMethod()]
+        public void InvokeJavascriptTest_SimpleJSCommand()
+        {
+            //Arrange
+            string url = "https://www.google.com";
+            var expectedResult = "Action has been successfully performed on the website.";
+            var jsToRun = "javascript:alert('Hello World');";
+            BrowserSetActions browserSetActions = new BrowserSetActions(uiActions, engineActions);
+
+            //Act
+            var result = browserSetActions.InvokeJavascript(url, jsToRun);
+
+            //Assert
+            Assert.IsTrue(result.Contains(expectedResult), "The result does not contain the expected success message.");
+        }
+    }
+}
