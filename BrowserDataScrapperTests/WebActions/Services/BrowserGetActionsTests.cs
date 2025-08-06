80bae12fabbd64641fbb9fcfbc1411c22ce3525d
MichalBudz1993 <m.budziszewski93@gmail.com>
2025-08-06 14:04:40 +0200

    tests(GetSet): Increased unit tests coverage

--- BrowserDataScrapperTests/WebActions/Services/BrowserGetActionsTests.cs
+++ BrowserDataScrapperTests/WebActions/Services/BrowserGetActionsTests.cs
@@ -2,6 +2,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BrowserDataScrapper.AutomationActions;
 using BrowserDataScrapper.AutomationActions.Services;
+using BrowserDataScrapper.AutomationActions.Interfraces;
+using BrowserDataScrapper.WebActions.Interfaces;
+using Moq;
+using System.Reflection;
+using System;
+using System.Windows.Forms;
 
 namespace BrowserDataScrapper.WebActions.Services.Tests
 {
@@ -25,5 +31,59 @@ namespace BrowserDataScrapper.WebActions.Services.Tests
             Assert.IsNotNull(result);
             Assert.IsTrue(result.Contains(expectedPartOfResult), "The result does not contain the expected part of HTML content.");
         }
+
+        private Mock<IUIActions> _uiActionsMock;
+        private Mock<IBrowserGeneralActions> _browserGeneralActionsMock;
+        private TestableBrowserGetActions _getActions;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _uiActionsMock = new Mock<IUIActions>();
+            _browserGeneralActionsMock = new Mock<IBrowserGeneralActions>();
+            _getActions = new TestableBrowserGetActions();
+
+            // Inject mocks into private fields using reflection
+            typeof(BrowserGetActions)
+                .GetField("_uiActions", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(_getActions, _uiActionsMock.Object);
+            typeof(BrowserGetActions)
+                .GetField("_browserGeneralActions", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(_getActions, _browserGeneralActionsMock.Object);
+        }
+
+        [TestMethod]
+        public void GetPageSourceAsString_ReturnsClipboardText()
+        {
+            string url = "https://www.google.com/";
+            string expected = "<html>";
+            Clipboard.SetText(expected);
+
+            _uiActionsMock.Setup(x => x.SetBrowserBarValue(It.IsAny<string>(), It.IsAny<string>())).Returns(string.Empty);
+
+            var result = _getActions.GetPageSourceAsString(url);
+
+            _browserGeneralActionsMock.Verify(x => x.PrepareBrowserInstance(url, 4000), Times.Once);
+            _uiActionsMock.Verify(x => x.SetBrowserBarValue(It.IsAny<string>(), "Chrome_WidgetWin_1"), Times.Once);
+            Assert.IsTrue(result.Contains(expected));
+        }
+
+
+        [TestMethod]
+        public void GetQuery_WhenException_ReturnsErrorMessage()
+        {
+            string url = "https://www.google.com/";
+            string query = "bad query";
+            _browserGeneralActionsMock.Setup(x => x.PrepareBrowserInstance(url, 4000)).Throws(new Exception("fail"));
+
+            // Use reflection to invoke private GetQuery
+            var method = typeof(BrowserGetActions).GetMethod("GetQuery", BindingFlags.NonPublic | BindingFlags.Instance);
+            var result = (string)method.Invoke(_getActions, new object[] { url, query, 4000 });
+
+            StringAssert.StartsWith(result, "Exception occured when downloading page source: ");
+        }
+
+        // Helper subclass to allow instantiation
+        private class TestableBrowserGetActions : BrowserGetActions { }
     }
 }
--- BrowserDataScrapperTests/WebActions/Services/BrowserSetActionsTests.cs
+++ BrowserDataScrapperTests/WebActions/Services/BrowserSetActionsTests.cs
@@ -2,30 +2,94 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BrowserDataScrapper.AutomationActions.Services;
 using BrowserDataScrapper.AutomationActions;
+using BrowserDataScrapper.AutomationActions.Interfraces;
+using BrowserDataScrapper.WebActions.Interfaces;
+using Moq;
 
 namespace BrowserDataScrapper.WebActions.Services.Tests
 {
-    [TestClass()]
-    public class BrowserSetActionsTests
-    {
 
-        [TestMethod()]
-        public void InvokeJavascriptTest_SimpleJSCommand()
+        [TestClass]
+        public class BrowserSetActionsTests
         {
-            //Arrange
-            string url = "https://www.google.com";
-            var expectedResult = "Action has been successfully performed on the website.";
-            var jsToRun = "javascript:alert('Hello World');";
-            BrowserGeneralActions browserGeneralActions = new BrowserGeneralActions();
-            BrowserSetActions browserSetActions = new BrowserSetActions();
+            private Mock<IUIActions> _uiActionsMock;
+            private Mock<IBrowserGeneralActions> _browserGeneralActionsMock;
+            private TestableBrowserSetActions _setActions;
 
-            //Act
-            var result = browserSetActions.InvokeJavascript(url, jsToRun);
+            [TestInitialize]
+            public void SetUp()
+            {
+                _uiActionsMock = new Mock<IUIActions>();
+                _browserGeneralActionsMock = new Mock<IBrowserGeneralActions>();
+                _setActions = new TestableBrowserSetActions(_uiActionsMock.Object, _browserGeneralActionsMock.Object);
+            }
 
-            //Assert
-            Assert.IsTrue(result.Contains(expectedResult), "The result does not contain the expected success message.");
+            [TestMethod]
+            public void InvokeJavascript_CorrectFormat_CallsDependenciesAndReturnsSuccess()
+            {
+                string url = "http://test";
+                string command = "alert('hi');";
+                _uiActionsMock.Setup(x => x.SetBrowserBarValue(It.IsAny<string>(), It.IsAny<string>())).Returns(string.Empty);
+
+                var result = _setActions.InvokeJavascript(url, command);
+
+                _browserGeneralActionsMock.Verify(x => x.PrepareBrowserInstance(url, 4000), Times.Once);
+                _uiActionsMock.Verify(x => x.SetBrowserBarValue("javascript:" + command, "Chrome_WidgetWin_1"), Times.Once);
+                Assert.AreEqual("Action has been successfully performed on the website.", result);
+            }
+
+            [TestMethod]
+            public void InvokeJavascript_IncorrectFormat_ReturnsIncorrectJS()
+            {
+                string url = "http://test";
+                string command = "alert('hi')"; // missing semicolon
+
+                var result = _setActions.InvokeJavascript(url, command);
+
+                Assert.AreEqual("Javascript is incorrect", result);
+                _uiActionsMock.Verify(x => x.SetBrowserBarValue(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+            }
+
+            [TestMethod]
+            public void SetElementById_CallsPrepareAndSetBrowserBarValue()
+            {
+                string url = "http://test";
+                string id = "input1";
+                string value = "testValue";
+                string expectedJs = $"javascript:(function(){{document.getElementById('{id}').value = '{value}';}})();";
+
+                _setActions.SetElementById(url, id, value);
+
+                _browserGeneralActionsMock.Verify(x => x.PrepareBrowserInstance(url, 4000), Times.Once);
+                _uiActionsMock.Verify(x => x.SetBrowserBarValue(expectedJs, "Chrome_WidgetWin_1"), Times.Once);
+            }
+
+            [TestMethod]
+            public void ClickOnElementById_CallsPrepareAndSetBrowserBarValue()
+            {
+                string url = "http://test";
+                string id = "btn1";
+                string expectedJs = $"javascript:(function(){{document.getElementById('{id}').click();}})();";
+
+                _setActions.ClickOnElementById(url, id);
+
+                _browserGeneralActionsMock.Verify(x => x.PrepareBrowserInstance(url, 4000), Times.Once);
+                _uiActionsMock.Verify(x => x.SetBrowserBarValue(expectedJs, "Chrome_WidgetWin_1"), Times.Once);
+            }
+
+            // Helper subclass to inject mocks into the private fields
+            private class TestableBrowserSetActions : BrowserSetActions
+            {
+                public TestableBrowserSetActions(IUIActions uiActions, IBrowserGeneralActions browserGeneralActions)
+                {
+                    typeof(BrowserSetActions)
+                        .GetField("_uiActions", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                        .SetValue(this, uiActions);
+                    typeof(BrowserSetActions)
+                        .GetField("_browserGeneralActions", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                        .SetValue(this, browserGeneralActions);
+                }
+            }
         }
 
-        
     }
-}
